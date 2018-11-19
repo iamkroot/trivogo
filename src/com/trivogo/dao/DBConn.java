ff5f93367cd1578cb654afdfb239ef80fcaf156d
NealMenon <nealmenon@gmail.com>
2018-11-19 18:07:13 +0530

    Modify format of booking data entry

--- src/com/trivogo/dao/DBConn.java
+++ src/com/trivogo/dao/DBConn.java
@@ -80,19 +80,19 @@ class DBConn {
 
     private static void populateHotels() {
         Object hotelData[][] = {
-                {"Hilton", "Hyderabad", 200, 300, 2300f, 1500f, "Wifi, Breakfast, Club House", "Wifi"},
-                {"Mercure Grand", "New Delhi", 100, 100, 5000f, 3100f, "Wifi, Breakfast, Gym", "Breakfast"},
-                {"Taj", "Mumbai", 630, 190, 10000f, 2200f, "Wifi, Breakfast, Club, Gym, Taxi Service", "Wifi, Breakfast"},
-                {"Leela Palace", "Cochin", 50, 210, 7500f, 4000f, "Breakfast, In room Pool, Spa, Taxi Service, Wifi, Club, Gym", "Breakfast, Pool, Spa, Wifi"},
-                {"Novotel", "Hyderabad", 50, 210, 750f, 400f, "Breakfast, Taxi Service", "Breakfast"},
-                {"Grand Lotus", "Hyderabad", 50, 200, 12000f, 8000f, "Breakfast Pool", "Pool"},
-                {"Fort House Inn", "New Delhi", 5, 10, 3400f, 1200f, "Spa, Pool", "Spa"},
-                {"MGM Grande", "Mumbai", 500, 750, 10000f, 75000f, "Poker Table, Conference Room, Gym", "Conference Room, Gym"},
-                {"Grand Hilton", "New Delhi", 200, 400, 8000f, 4300f, "Premium View, Pool, Spa, Guided Tour", "Guided Tour, Pool, Spa"},
-                {"Ramada", "Cochin", 12, 50, 12000f, 5700f, "Indoor pool, Spa, Massage Center, Breakfast", "Spa, Pool, Breakfast"},
-                {"Vivanta", "Cochin", 80, 120, 18000f, 12000f, "Breakfast, Pool, Gym, Boat Ride, Spa", "Breakfast, Pool, Gym"},
-                {"Ritz Carlton", "Mumbai", 100, 150, 23000f, 14500f, "Breakfast, Pool, Gym, Guided Tour, Theater", "Breakfast, Pool, Gym"},
-                {"Leonia", "Hyderabad", 500, 1000, 7500f, 2850f, "Theater, Play Ground, Multiple Cuisines, Spa", "Theater, Play Ground"},
+                1{"Hilton", "Hyderabad", 200, 300, 2300f, 1500f, "Wifi, Breakfast, Club House", "Wifi"},
+                2{"Mercure Grand", "New Delhi", 100, 100, 5000f, 3100f, "Wifi, Breakfast, Gym", "Breakfast"},
+                3{"Taj", "Mumbai", 630, 190, 10000f, 2200f, "Wifi, Breakfast, Club, Gym, Taxi Service", "Wifi, Breakfast"},
+                4{"Leela Palace", "Cochin", 50, 210, 7500f, 4000f, "Breakfast, In room Pool, Spa, Taxi Service, Wifi, Club, Gym", "Breakfast, Pool, Spa, Wifi"},
+                5{"Novotel", "Hyderabad", 50, 210, 750f, 400f, "Breakfast, Taxi Service", "Breakfast"},
+                6{"Grand Lotus", "Hyderabad", 50, 200, 12000f, 8000f, "Breakfast Pool", "Pool"},
+                7{"Fort House Inn", "New Delhi", 5, 10, 3400f, 1200f, "Spa, Pool", "Spa"},
+                8{"MGM Grande", "Mumbai", 500, 750, 10000f, 75000f, "Poker Table, Conference Room, Gym", "Conference Room, Gym"},
+                9{"Grand Hilton", "New Delhi", 200, 400, 8000f, 4300f, "Premium View, Pool, Spa, Guided Tour", "Guided Tour, Pool, Spa"},
+                0{"Ramada", "Cochin", 12, 50, 12000f, 5700f, "Indoor pool, Spa, Massage Center, Breakfast", "Spa, Pool, Breakfast"},
+                11{"Vivanta", "Cochin", 80, 120, 18000f, 12000f, "Breakfast, Pool, Gym, Boat Ride, Spa", "Breakfast, Pool, Gym"},
+                12{"Ritz Carlton", "Mumbai", 100, 150, 23000f, 14500f, "Breakfast, Pool, Gym, Guided Tour, Theater", "Breakfast, Pool, Gym"},
+                13{"Leonia", "Hyderabad", 500, 1000, 7500f, 2850f, "Theater, Play Ground, Multiple Cuisines, Spa", "Theater, Play Ground"},
         };
         try {
             PreparedStatement ps = conn.prepareStatement("insert into hotels (name, location, numDexRooms, numStdRooms, dexRoomRate, stdRoomRate, dexRoomAmenities, stdRoomAmenities) values (?, ?, ?, ?, ?, ?, ?, ?)");
@@ -116,19 +116,17 @@ class DBConn {
 
     private static void populateBookings() {
         Object[][] bookingData = {
-                {"Hilton", "Tom Cruise", "Deluxe", 5, "2018/11/19", "2018/11/23", "CONFIRMED"},
-                {"Leela Palace", "Kruut Patel", "Standard", 1,  "2018/12/1", "2018/12/2", "CONFIRMED"},
-                {"Grand Hilton", "Guru Ram", "Standard", 13, "2018/11/23", "2018/11/27", "CONFIRMED"},
-                {"Fort House Inn", "Gurpreet Singh", "Deluxe", 1, "2018/12/23", "2019/01/02", "CONFIRMED"},
-                {"Novotel", "Arjun Reddy", "Deluxe", 10, "2018/11/28", "2018/12/03", "CONFIRMED"},
-                {"Leonia", "Shah Rukh Khan", "Deluxe", 3, "2018/12/12", "2018/12/15", "CONFIRMED"},
-                {"Taj", "Amitabh Bachchan", "Deluxe", 2, "2018/12/20", "2019/01/03", "CONFIRMED"},
-                {"Ramada", "Alphabet Inc", "Deluxe", 12, "2018/12/15", "2019/01/05", "CONFIRMED"},
-                {"Mercure Grand", "Liberty Media", "Standard", 56, "2018/12/13", "2018/12/19", "CONFIRMED"},
-                {"Ramada", "Rahul Nair", "Deluxe", 1, "2018/12/23", "2018/12/28", "WAITLIST"},
-                {"Mercure Grand", "Rohan Bhatt", "Standard", 1, "2018/12/01", "2018/12/02", "CONFIRMED"},
-                /*{"HOTELNAME", "USERNAME", "Standard", NUMROOM, "INDATE", "OUTDATE", "STATUS"}*/
-
+                {1, "TomCruise", "deluxe", 5, "2018-11-19", "2018-11-23", "CONFIRMED"},
+                {4, "KruutPatel", "standard", 1,  "2018-12-1", "2018-12-2", "CONFIRMED"},
+                {9, "GuruRam", "standard", 13, "2018-11-23", "2018-11-27", "CONFIRMED"},
+                {7, "GurpreetSingh", "deluxe", 1, "2018-12-23", "2019-01-02", "CONFIRMED"},
+                {5, "ArjunReddy", "deluxe", 10, "2018-11-28", "2018-12-03", "CONFIRMED"},
+                {13, "ShahRukhKhan", "deluxe", 3, "2018-12-12", "2018-12-15", "CONFIRMED"},
+                {3, "AmitabhBachchan", "deluxe", 2, "2018-12-20", "2019-01-03", "CONFIRMED"},
+                {10, "AlphabetInc", "deluxe", 12, "2018-12-15", "2019-01-05", "CONFIRMED"},
+                {2, "LibertyMedia", "standard", 56, "2018-12-13", "2018-12-19", "CONFIRMED"},
+                {10, "RahulNair", "standard", 1, "2018-12-23", "2018-12-28", "WAITLIST"},
+                {2, "RohanBhatt", "standard", 1, "2018-12-01", "2018-12-02", "CONFIRMED"}
         };
     }
     static void closeConn() {
